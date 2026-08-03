9b7f3c3c0e59795c8405e9bcc68e669c6384a669
farmanullah22 <eng.farmanullah00@gmail.com>
2026-08-02 18:57:12 -0700

    logo used in login also inside Dashboard

--- mobile/lib/core/widgets/app_logo.dart
+++ mobile/lib/core/widgets/app_logo.dart
@@ -13,42 +13,11 @@ class AppLogo extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Container(
+        Image.asset(
+          'lib/images/logo.png',
           width: size,
           height: size,
-          decoration: BoxDecoration(
-            gradient: const LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [AppColors.primary, AppColors.primaryDark],
-            ),
-            borderRadius: BorderRadius.circular(size * 0.28),
-            boxShadow: [
-              BoxShadow(
-                color: AppColors.primary.withValues(alpha: 0.35),
-                blurRadius: size * 0.4,
-                offset: Offset(0, size * 0.12),
-              ),
-            ],
-          ),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Icon(Icons.grid_view_rounded, size: size * 0.52, color: AppColors.accent),
-              Positioned(
-                bottom: size * 0.12,
-                right: size * 0.14,
-                child: Container(
-                  width: size * 0.22,
-                  height: size * 0.22,
-                  decoration: const BoxDecoration(
-                    color: AppColors.accent,
-                    shape: BoxShape.circle,
-                  ),
-                ),
-              ),
-            ],
-          ),
+          fit: BoxFit.contain,
         ),
         SizedBox(height: size * 0.18),
         Text(
